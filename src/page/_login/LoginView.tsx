026a9caa1632ccef50cc82dc776979e6b978e188
unknown <lunatics384@gmail.com>
2021-12-17 22:29:49 +0900

    [1217] 로그인뷰 Input 컴포넌트 useCallback 적용

--- src/page/_login/LoginView.tsx
+++ src/page/_login/LoginView.tsx
@@ -1,26 +1,36 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useCallback} from 'react';
 
 const InputEmail = ({handleInput}) => {
     return(
         <div>
-            <input className={"email"} type={"text"} onChange={handleInput} />
+            <input name={"email"} type={"text"} onChange={handleInput} />
         </div>
     )
 }
 const InputPassword = ({handleInput}) => {
     return(
         <div>
-            <input className={"password"} type={"password"} onChange={handleInput} />
+            <input name={"password"} type={"password"} onChange={handleInput} />
         </div>
     )
 }
 
-const LoginView:React.FC = () => {
 
-    const handleInput = ({target}) => {
-        const {value, className} = target;
-        console.log(value, className);
-    }
+const LoginView:React.FC = () => {
+    const [inputValues, setInputValues] = useState({email: "", password: ""});
+
+    useEffect(() => {
+        console.log(`${__filename} ::: LoginView Mounted`);
+    })
+
+    const handleInput = useCallback( ({target}) => {
+            const {value, name} = target;
+            setInputValues( {
+                ...inputValues, [name] : value,
+            });
+            console.log(inputValues);
+    }, [inputValues])
+
     return(
         <>
             <InputEmail
